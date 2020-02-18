33009360cf328914432a9035d8c2f68498cef3d3
hodeng28 <ghepd28@gmail.com>
2020-02-18 23:59:49 +0900

    js_study

---
+++ study_js/index.js
@@ -0,0 +1,20 @@
+function sumOf (numbers) {
+	let sum = 0;
+	for (let  I = 0;  i < numbers.length i++ ) {
+	 sum += numbers[ i ];
+      }
+	return sum;
+}
+
+const result = sumOf ([1, 2, 3, 4, 5]);
+console.log(result);
+
+// 3보다 큰 숫자로만 이루어진 배열을 새로 만들어서 반환하기
+
+function biggerThanThree (numbers) {
+
+}
+
+const numbers = [1,2,3,4,5,6,7]
+console.log(biggerThanThree(numbers));
+
