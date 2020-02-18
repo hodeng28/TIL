77e14962fb94342eb9cc5eefc2da10491e4cc2b2
hodeng28 <ghepd28@gmail.com>
2020-02-19 01:15:14 +0900

    study_js

--- study_js/index.js
+++ study_js/index.js
@@ -18,3 +18,21 @@ function biggerThanThree (numbers) {
 const numbers = [1,2,3,4,5,6,7]
 console.log(biggerThanThree(numbers));
 
+//답
+
+function biggerThanThree(numbers) {
+	const array = [];
+	for (let i = 0; i < numbers.length; i++) {
+	  if (numbers[i] > 3) {
+		array.push(numbers[i]);
+	  }
+	}
+	return array;
+  }
+  
+  const numbers = [1, 2, 3, 4, 5, 6, 7];
+  console.log(biggerThanThree(numbers)); // [4, 5, 6, 7]
+  
+  
+  
+
