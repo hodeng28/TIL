267a0e8cd84b1d059954db931a18ed888c493113
hodeng28 <ghepd28@gmail.com>
2020-04-15 02:39:03 +0900

    implement Analog Clock

--- study_js/ex/Analog Clock/ex.js
+++ study_js/ex/Analog Clock/ex.js
@@ -5,7 +5,7 @@ const $digitalClock = document.querySelector('.digital-clock');
 const $hand = document.querySelector('.hand')
 
 const $hourHand = document.querySelector('.hour');
-const $minuteHand = document.querySelector('.hour');
+const $minuteHand = document.querySelector('.minute');
 const $secondHand = document.querySelector('.second');
 
 
@@ -20,15 +20,16 @@ function digitalTimer() {
 
       const hour = today.getHours();
       const min = today.getMinutes();
-      const sec = today.getSeconds();
+      let sec = today.getSeconds();
 
+      $hourHand.style.transform = `rotate(${hour*30}deg)`
+      $minuteHand.style.transform = `rotate(${min*6}deg)`
+      $secondHand.style.transform = `rotate(${sec*6}deg)`
       
       
       $digitalClock.innerHTML = `${hour < 10 ? `0${hour}` : hour} : ${min < 10 ? `0${min}` : min} : ${sec < 10? `0${sec}` : sec}`
-    }, 1); 
-    
-      $secondHand.style.transform =  `rotate(${}deg)`    
+    }, 1000); 
+
 };
 digitalTimer();
-
 
