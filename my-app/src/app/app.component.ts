786a7b3f02efa2af7b05412aac903484b179e745
hodeng28 <ghepd28@gmail.com>
2020-04-17 22:01:59 +0900

    pratice angular

---
+++ my-app/src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `<todos></todos>`  
+})
+export class AppComponent {
+  title = 'my-app';
+}
---
+++ my-app/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { XyzComponent } from './xyz/xyz.component';
+import { TodosComponent } from './todos/todos.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    XyzComponent,
+    TodosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
