1cec308c08441139d9d357f60ced449f652df11d
hodeng28 <ghepd28@gmail.com>
2020-04-26 00:50:05 +0900

    fix: code html, css, js

--- study_js/ex/Popup UI/ex.js
+++ study_js/ex/Popup UI/ex.js
@@ -1,7 +1,15 @@
 const $popup = document.querySelector('.popup');
 const $togglePopup = document.querySelector('.toggle-popup');
+const $btnClose = document.querySelector('.btn-close');
+const $btnCancel =document.querySelector('.btn-cancel');
 
 
+const removePopUp = () => $popup.classList.toggle('on');
 
+$togglePopup.addEventListener('click', removePopUp);
+
+$btnClose.addEventListener('click', removePopUp);
+
+$btnCancel.addEventListener('click', removePopUp);
 
 
