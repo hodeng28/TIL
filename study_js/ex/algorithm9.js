22edfe5b9c30074a4c54ff73da78f79cb455f08c
hodeng28 <ghepd28@gmail.com>
2020-05-06 19:53:30 +0900

    study: js

--- study_js/ex/algorithm9.js
+++ study_js/ex/algorithm9.js
@@ -2,7 +2,12 @@
 // 예를 들어 인수가 [3, 6, -2, -5, 7, 3]인 경우, 21을 반환한다.
 
 function adjacentElementsProduct(arr) {
-  
+
+  let max = [];
+  for (let i = 0; i < arr.length - 1; i++) {
+    max.push(arr[i] * arr[i + 1])
+  }
+  return Math.max(...max);
 }
 
-console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3])); 
+console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
