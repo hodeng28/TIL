396d380db49d29039db7c826f20e07a407646bf9
hodeng28 <ghepd28@gmail.com>
2020-05-11 20:11:11 +0900

    implement: login form

--- study_React/chapter03/src/App.js
+++ study_React/chapter03/src/App.js
@@ -1,26 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import classNames from 'classnames/bind';
+import style from './App.css';
 
-function App() {
+const st = classNames.bind(style);
+
+const App = () => {
+  const submit = (e) => {
+    e.preventDefault();
+    console.log('로그인 시도중');
+  };
+
+  const logID = () => {
+    console.log('아이디 입력중');
+  };
+
+  const logPass = () => {
+    console.log('비밀번호 입력중');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={st('main')}>
+      <div>
+        <h1 className={st('header')}>로그인</h1>
+      </div>
+      <div className={st('App', 'blue')}>
+        <form className={st('formStyle')} onSubmit={submit}>
+          <h3>ID :</h3>
+          <input
+            type="text"
+            className={st('input')}
+            placeholder="아이디를 입력하세요"
+            onChange={logID}
+          />
+          <h3>PASS :</h3>
+          <input
+            type="text"
+            className={st('input')}
+            placeholder="비밀번호를 입력하세요"
+            onChange={logPass}
+          />
+          <button type="submit">로그인하기</button>
+        </form>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
