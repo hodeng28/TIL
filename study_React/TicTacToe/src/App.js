8f4c973b8ee0bbf084e65d26ca4e394d8e4d0829
hodeng28 <ghepd28@gmail.com>
2020-05-15 01:33:00 +0900

    init

--- study_React/TicTacToe/src/App.js
+++ study_React/TicTacToe/src/App.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
 
+  const container = () => {
+    const [play, setPlay] = useState();
+    const [state, setState] = useState({
+      a1: "",
+      a2: "",
+      a3: "",
+      a4: "",
+      a5: "",
+      a6: "",
+      a7: "",
+      a8: "",
+      a9: "",
+    });
+  };
+  const result = () => {
+
+  }
+
+
+  const onClick = () => {
+    return "x" ? "o" : "x";
+  }
 
 
 
@@ -10,19 +32,19 @@ function App() {
 
     <div className="container">
       <ul>
-        <li></li>
-        <li></li>
-        <li></li>
+        <li className={"a1"} onClick={onClick}></li>
+        <li className={"a2"} onClick={onClick}></li>
+        <li className={"a3"} onClick={onClick}></li>
       </ul>
       <ul>
-        <li></li>
-        <li></li>
-        <li></li>
+        <li className={"a4"} onClick={onClick}></li>
+        <li className={"a5"} onClick={onClick}></li>
+        <li className={"a6"} onClick={onClick}></li>
       </ul>
       <ul>
-        <li></li>
-        <li></li>
-        <li></li>
+        <li className={"a7"} onClick={onClick}></li>
+        <li className={"a8"} onClick={onClick}></li>
+        <li className={"a9"} onClick={onClick}></li>
       </ul>
     </div>
   );
