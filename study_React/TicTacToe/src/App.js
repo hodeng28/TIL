be18339015b5529132159df856a7c5e12f48152b
hodeng28 <ghepd28@gmail.com>
2020-05-14 22:53:13 +0900

    init

---
+++ study_React/TicTacToe/src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+
+
+
+
+  return (
+
+    <div className="container">
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
