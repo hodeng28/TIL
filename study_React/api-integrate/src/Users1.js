1e3490ccc9ab2b1797aace5ddc794c198cf199d4
hodeng28 <ghepd28@gmail.com>
2020-05-19 21:48:23 +0900

    pratice: api using hook

--- study_React/api-integrate/src/Users1.js
+++ study_React/api-integrate/src/Users1.js
@@ -1,32 +1,17 @@
-import React, { useEffect, useReducer } from 'react';
+import React from 'react';
 import axios from 'axios';
-import { asyncReducer } from './asyncReducer';
+import useAsync from './useAsync';
 
+async function getUsers() {
+  const response = await axios.get('http://jsonplaceholder.typicode.com/users/');
+  return response.data;
+}
 function Users1() {
-  const [state, dispatch] = useReducer(asyncReducer, {
-    loading: false,
-    data: null,
-    error: null,
-  });
-  const fetchUsers = async () => {
-    dispatch({ type: 'LOADING' });
-    try {
-      const response = await axios.get(
-        'http://jsonplaceholder.typicode.com/users/'
-      );
-      dispatch({ type: 'SUCCESS', data: response.data });
-    } catch (e) {
-      dispatch({ type: 'ERROR', error: e });
-    }
-  };
-
-  useEffect(() => {
-    fetchUsers();
-  }, []);
+  const [state, refetch] = useAsync(getUsers, [], true);
   const { loading, error, data: users } = state;
   if (loading) return <div>로딩중</div>
   if (error) return <div>에러발생</div>
-  if (!users) return null;
+  if (!users) return <button onClick={refetch}>불러오기</button>;
 
   return (
     <>
@@ -37,7 +22,7 @@ function Users1() {
           </li>
         ))}
       </ul>
-      <button onClick={fetchUsers}>다시 불러오기</button>
+      <button onClick={refetch}>다시 불러오기</button>
     </>
   );
 }
