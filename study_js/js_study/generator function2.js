c0fb919713fc3326b2d6791bd8ceb40fe23e3642
hodeng28 <ghepd28@gmail.com>
2020-06-05 17:01:37 +0900

    finobacci generator

--- study_js/js_study/generator function2.js
+++ study_js/js_study/generator function2.js
@@ -20,3 +20,21 @@ for (const num of infiniteFibonacci) {
   if (num > 1000) break;
   console.log(num);
 }
+
+
+
+// 제너레이터를 활용하여 피보나치 수열을 구현한 이터러블 
+
+const infiFibonacci = (function* () {
+  let [pre, cur] = [0, 1];
+
+  while (true) {
+    [pre, cur] = [cur, pre + cur];
+    yield cur;
+  }
+}());
+
+for (const num of infiFibonacci) {
+  if (num > 1000) break;
+  console.log(num);
+}
