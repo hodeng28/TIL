1a45ecfd2beffbfcca41fbfd25bf77741378e6b0
hodeng28 <ghepd28@gmail.com>
2020-06-06 22:35:00 +0900

    redux-thunk

--- study_React/redux_middleware/src/index.js
+++ study_React/redux_middleware/src/index.js
@@ -7,11 +7,13 @@ import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './modules';
 import { composeWithDevTools } from 'redux-devtools-extension';
-
 import logger from 'redux-logger';
+import ReduxThunk from 'redux-thunk';
+
 
 const store = createStore(rootReducer,
-  composeWithDevTools(applyMiddleware(logger)));
+  composeWithDevTools(applyMiddleware(ReduxThunk, logger)));
+// logger를 사용하는 경우 logger는 가장 마지막에 들어가야한다.
 
 ReactDOM.render(
   <Provider store={store}>
---
+++ study_React/redux_middleware/src/modules/posts.js
@@ -0,0 +1,55 @@
+import * as postsAPI from '../api/posts';
+import { reducerUtils, createPromiseThunk } from '../lib/asyncUtils';
+
+const GET_POSTS = 'GET_POSTS';
+const GET_POSTS_SUCCESS = 'GET_POSTS_SUCCESS';
+const GET_POSTS_ERROR = 'GET_POSTS_ERROR';
+
+const GET_POST = 'GET_POST';
+const GET_POST_SUCCESS = 'GET_POST_SUCCESS';
+const GET_POST_ERROR = 'GET_POST_ERROR';
+
+export const getPosts = createPromiseThunk(GET_POSTS, postsAPI.getPosts);
+export const getPost = createPromiseThunk(GET_POST, postsAPI.getPostById);
+
+const initialState = {
+  posts: reducerUtils.initial(),
+  post: reducerUtils.initial()
+};
+
+export default function posts(state = initialState, action) {
+  switch (action.type) {
+    case GET_POSTS:
+      return {
+        ...state,
+        posts: reducerUtils.loading()
+      };
+    case GET_POSTS_SUCCESS:
+      return {
+        ...state,
+        posts: reducerUtils.loading(action.payload)
+      };
+    case GET_POSTS_ERROR:
+      return {
+        ...state,
+        posts: reducerUtils.loading(action.payload)
+      };
+    case GET_POST:
+      return {
+        ...state,
+        post: reducerUtils.loading()
+      };
+    case GET_POST_SUCCESS:
+      return {
+        ...state,
+        post: reducerUtils.loading(action.payload)
+      };
+    case GET_POST_ERROR:
+      return {
+        ...state,
+        post: reducerUtils.loading(action.payload)
+      };
+    default:
+      return state;
+  }
+}
