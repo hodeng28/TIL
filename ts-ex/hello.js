2d21616e6ad7647f30f0ee235a899c2a05d86b79
hodeng28 <ghepd28@gmail.com>
2020-06-09 21:13:43 +0900

    first TypeScript

---
+++ ts-ex/hello.js
@@ -0,0 +1,8 @@
+var hello = "hello";
+let hello2 = "hello2";
+let timeoutPromise = new Promise((reslove, reject) => {
+    setTimeout(() => {
+        reslove("1 sec");
+    }, 1000);
+});
+timeoutPromise.then(console.log);
