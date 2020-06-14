b18ced2a2da99c579814e8edb34997aa20d87efd
hodeng28 <ghepd28@gmail.com>
2020-06-14 10:45:16 +0900

    algorithm_study

---
+++ algorithm_study/src/2.js
@@ -0,0 +1,47 @@
+class ArrayList {
+  constructor(capacity) {
+    this.capacity = capacity;
+    this.array = new Int32Array(capacity);
+    this.length = 0;
+  }
+
+  isEmpty() {
+    if (this.length === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  prepend(value) {
+    if (this.array.length !== 0) {
+      return this. *= 2;
+    }
+  }
+  //
+
+  append(value) {
+
+  }
+
+  setHead(index) {
+
+  }
+
+  access(index) {
+
+  }
+
+  insert(index, value) {
+
+  }
+
+  remove(index) {
+
+  }
+
+  print() {
+
+  }
+}
+
