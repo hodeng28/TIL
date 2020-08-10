4de920c027d9db8f2cc8c51f031328d8bb2c35a0
hodeng28 <ghepd28@gmail.com>
2020-08-11 00:14:56 +0900

    ex SEO, starbuck mainpage

--- SEO/src/App.js
+++ SEO/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.css';
+import Header from './Components/Header';
+import Main from './Components/Main';
 
 function App() {
   return (
-    <div></div>
-  )
+    <>
+      <Header />
+      <Main />
+    </>
+  );
 }
 export default App;
