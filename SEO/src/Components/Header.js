5a8e822e04aad1f62b600552387b4fe7fa34ffc5
hodeng28 <ghepd28@gmail.com>
2020-08-12 21:08:40 +0900

    header, main html css

--- SEO/src/Components/Header.js
+++ SEO/src/Components/Header.js
@@ -1,8 +1,37 @@
 import React from "react";
+import "./style/Header.scss"
 
 const Header = () => {
   return (
-    <div>헤더</div>
+    <>
+      <div className="header">
+        <div className="headerWrap">
+          <h1 className="logo">
+            <img
+              className="mainLogo"
+              alt="starbuck logo"
+              src="https://www.starbucks.co.kr/common/img/common/logo.png"
+            />
+          </h1>
+          <div className="headerMenu">
+            <ul className="subNav">
+              <li>Sign In</li>
+              <li>My StarBucks</li>
+              <li>Customer Service Ideas</li>
+              <li>Find a Store</li>
+            </ul>
+            <ul className="headerNav">
+              <li>COFFEE</li>
+              <li>MENU</li>
+              <li>STORE</li>
+              <li>RESPONSIBILITY</li>
+              <li>MY STARBUCKS REWARDS</li>
+              <li>WHAT'S NEW</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
--- SEO/src/Components/Main.js
+++ SEO/src/Components/Main.js
@@ -1,8 +1,60 @@
 import React from "react";
+import "./style/Main.scss"
 
 const Main = () => {
   return (
-    <div>메인</div>
+    <div>
+      <div className="mainBg">
+        <main class="main">
+          <section class="mainEventWrap">
+            <div class="mainEvent">
+              <div class="mainTitle">
+                <img
+                  src="https://image.istarbucks.co.kr/upload/common/img/main/2020/20_summer3_emblem.png"
+                  alt=""
+                />
+                <button>자세히보기</button>
+              </div>
+              <ul class="mainNewMenu">
+                <li class="newMenu1">
+                  <img
+                    src="https://image.istarbucks.co.kr/upload/common/img/main/2020/summer3_bev1.png"
+                    alt="블론드 서머 라떼 이미지"
+                  />
+                  <img
+                    src="https://image.istarbucks.co.kr/upload/common/img/main/2020/summer3_bev1_txt.png"
+                    alt="블론드 서머 라떼 텍스트"
+                    className="newMenuText1"
+                  />
+                </li>
+                <li class="newMenu2">
+                  <img
+                    src="https://image.istarbucks.co.kr/upload/common/img/main/2020/summer3_bev2.png"
+                    alt="스위티 자몽 라임 블렌디드 이미지"
+                  />
+                  <img
+                    src="https://image.istarbucks.co.kr/upload/common/img/main/2020/summer3_bev2_txt.png"
+                    alt="스위티 자몽 라임 블렌디드 텍스트"
+                    className="newMenuText2"
+                  />
+                </li>
+                <li class="newMenu3">
+                  <img
+                    src="https://image.istarbucks.co.kr/upload/common/img/main/2020/summer3_bev3.png"
+                    alt="블론드 서머 라떼 이미지"
+                  />
+                  <img
+                    src="https://image.istarbucks.co.kr/upload/common/img/main/2020/summer3_bev3_txt.png"
+                    alt="블론드 서머 라떼 텍스트"
+                    className="newMenuText3"
+                  />
+                </li>
+              </ul>
+            </div>
+          </section>
+        </main>
+      </div>
+    </div>
   );
 };
 
