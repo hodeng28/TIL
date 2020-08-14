065182062b618d1302a5428f06a0cc37ff437c07
hodeng28 <ghepd28@gmail.com>
2020-08-14 19:52:29 +0900

    pull

--- SEO/src/App.js
+++ SEO/src/App.js
@@ -2,10 +2,15 @@ import React from 'react';
 import './App.css';
 import Header from './Components/Header';
 import Main from './Components/Main';
+import { Helmet } from 'react-helmet';
+
 
 function App() {
   return (
     <>
+      <Helmet>
+        <title>asterbucks</title>
+      </Helmet>
       <Header />
       <Main />
     </>
