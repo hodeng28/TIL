f7650a514af1e3052b1850213d737613a26dcef8
hodeng28 <ghepd28@gmail.com>
2020-08-21 00:33:25 +0900

    pratice: vue

---
+++ study_Vue.js/vue-app/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <form v-on:submit="submitForm">
+      <div>
+        <label for="username">ID:</label>
+        <input 
+          id="username" 
+          type="text" 
+          v-model="username"
+        >
+      </div>
+      <div>
+        <label for="password">PW:</label>
+        <input 
+          id="password" 
+          type="password" 
+          v-model="password"
+        >
+      </div>
+      <button>로그인</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: '',
+      password: '',
+    }
+  },
+  methods: {
+    submitForm(e) {
+      e.preventDefault();
+      console.log('submitted');  
+    },
+  },
+};
+</script>
+
+<style>
+
+</style>
