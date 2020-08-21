bffea7803fbd88208fb7b525510cfeb495357fec
hodeng28 <ghepd28@gmail.com>
2020-08-21 18:02:38 +0900

    feat: toastPopup

--- study_Vue.js/vue-app/src/App.vue
+++ study_Vue.js/vue-app/src/App.vue
@@ -26,11 +26,19 @@
         로그인
       </button>
     </form>
-    <p v-if="isError">올바르지 않은 ID 입니다.</p>
+    <p v-if="isSuccess">로그인 성공하였습니다.</p>
     <p v-if="isUsernameValid">이메일 형식에 맞습니다.</p>
+    <ToastPopup 
+      v-bind:open="isSuccess"
+      v-on:close="isSuccess = false"
+    >
+    </ToastPopup>
   </div>
 </template>
+
 <script>
+import ToastPopup from '@/components/ToastPopup.vue';
+
 // email 형식 체크 함수
 function validateEmail(email) {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -38,11 +46,15 @@ function validateEmail(email) {
 }
 
 export default {
+  components: {
+    ToastPopup
+  },
   data() {
     return {
       username: '',
       password: '',
       isError: false,
+      isSuccess: false,
     };
   },
   computed: {
@@ -61,15 +73,17 @@ export default {
       //   .catch(error => {
       //     this.isError = true;
       //   })
-      // this.initForm();
+      this.isSuccess = true;
+      this.initForm();
     },
-    // initForm() {
-    //   this.username = '';
-    //   this.password = '';
-    // },
+    initForm() {
+      this.username = '';
+      this.password = '';
+    },
   },
 };
 </script>
+
 <style scoped>
 /* scoped - 현재 컴퍼넌트에만 적용 */
   .usernameInput {
