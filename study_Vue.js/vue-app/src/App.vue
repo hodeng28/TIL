668be1fc6e85e1b5f5ea4f3e03f2bdaadc4a036a
hodeng28 <ghepd28@gmail.com>
2020-08-21 14:40:07 +0900

    feat: logIn form

--- study_Vue.js/vue-app/src/App.vue
+++ study_Vue.js/vue-app/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <form v-on:submit="submitForm">
+    <form v-on:submit.prevent="submitForm">
       <div>
         <label for="username">ID:</label>
         <input 
@@ -17,28 +17,56 @@
           v-model="password"
         >
       </div>
-      <button>로그인</button>
+      <button 
+        v-bind:disabled="!isUsernameValid" 
+        type="submit"
+      >
+        로그인
+      </button>
     </form>
+    <p v-if="isError">올바르지 않은 ID 입니다.</p>
+    <p v-if="isUsernameValid">이메일 형식에 맞습니다.</p>
   </div>
 </template>
-
 <script>
+// email 형식 체크 함수
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
 export default {
   data() {
     return {
       username: '',
       password: '',
-    }
+      isError: false,
+    };
+  },
+  computed: {
+    isUsernameValid() {
+      return validateEmail(this.username);
+    } 
   },
   methods: {
-    submitForm(e) {
-      e.preventDefault();
-      console.log('submitted');  
+    submitForm() {
+      // e.preventDefault();
+      console.log('로그인완료');
+      this.isError = true;
+      // axios
+      //   .post()
+      //   .then()
+      //   .catch(error => {
+      //     this.isError = true;
+      //   })
+      this.initForm();
     },
+    initForm() {
+      this.username = '';
+      this.password = '';
+    }
   },
 };
 </script>
-
 <style>
-
 </style>
