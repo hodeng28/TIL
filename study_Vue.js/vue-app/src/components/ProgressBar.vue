de36c7b047fa34fcbdb4e73f238e63e1e48de542
hodeng28 <ghepd28@gmail.com>
2020-08-22 23:33:47 +0900

    feat: animation progressBar

---
+++ study_Vue.js/vue-app/src/components/ProgressBar.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="progress">
+    
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+/* scoped - 현재 컴퍼넌트에만 적용 */
+  .progress {
+    width: 0%;
+    height: 5px;
+    background-color: chocolate;
+    /* transition: width 700ms linear; */
+    animation: loading 1.5s linear;
+  }
+  /* .loadingBar {
+    width: 100%;
+  } */
+  @keyframes loading {
+    0% {
+      width: 0%;
+    }
+    50% {
+      width: 10%;
+    }
+    100% {
+      width: 20%;
+    }
+  }
+</style>
