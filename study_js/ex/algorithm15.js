717258333c9a6a87e1a2d48c5c4d7e1dca811b2e
hodeng28 <ghepd28@gmail.com>
2020-09-25 23:07:26 +0900

    pratice algorithm

--- study_js/ex/algorithm15.js
+++ study_js/ex/algorithm15.js
@@ -1,4 +1,4 @@
-// 핸드폰 번호 가리기
+// 핸드폰 번호 가리기 in programmers
 
 // 문제 설명
 // 프로그래머스 모바일은 개인정보 보호를 위해 고지서를 보낼 때 고객들의 전화번호의 일부를 가립니다.
@@ -23,3 +23,4 @@ function solution(phone_number) {
 
 console.log(solution("01033334444"));
 console.log(solution("01036300188"));
+
