5d249b26e3f39dcf395d9d7ffcf1c850831a6ccc
hodeng28 <ghepd28@gmail.com>
2020-12-13 10:27:34 +0900

    useInput

--- study_React/react_hook/src/App.js
+++ study_React/react_hook/src/App.js
@@ -1,20 +1,25 @@
 import React, { useState } from 'react';
 import ReactDom from 'react-dom';
 
-const useInput = initialValue => {
-  const [value, setValue] = useState(initialValue);
-  const onChange = e => {
-    console.log(e.target);
+const content = [
+  {
+    tab: "Section 1",
+    content: "the content of the Section1"
+  },
+  {
+    tab: "Section 2",
+    content: "the content of the Section2"
   }
-  return { value };
-}
+]
 
 const App = () => {
-  const name = useInput("Mr.")
+  
   return (
     <div className="App">
       <h1>study react hook</h1>
-      <input placeholder="Name" {...name} />
+        {content.map(section => (
+          <button>{section.tab}</button>
+        ))}
     </div>
   );
 }
