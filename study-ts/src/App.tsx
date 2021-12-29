e59cb2da8402e3b1f04f9925a8325a42e427edfa
hodeng28 <ghepd28@gmail.com>
2021-12-29 23:29:40 +0900

    pratice: useReducer

--- study-ts/src/App.tsx
+++ study-ts/src/App.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
-import MyForm from './MyForm';
+import Counter from './Counter';
+// import MyForm from './MyForm';
 // import Greetings from './Greeting';
 
 const App: React.FC =() => {
-  const onSubmit = (form: { name: string, description: string}) => {
-    console.log(form);
-  }
-  return (
-      <MyForm onSubmit={onSubmit} />
-  );
+  // const onSubmit = (form: { name: string, description: string}) => {
+  //   console.log(form);
+  // }
+  // return (
+  //     <MyForm onSubmit={onSubmit} />
+  // );
+
+  return <Counter />
 }
 
 export default App;
--- study-ts/src/Counter.tsx
+++ study-ts/src/Counter.tsx
@@ -1,10 +1,24 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
+
+type Action = { type: "INCREASE"} | { type: "DECREASE"};
+
+function reducer(state: number, action: Action): number {
+  switch (action.type) {
+    case 'INCREASE':
+      return state + 1;
+    case 'DECREASE':
+      return state - 1;
+  
+    default:
+      throw new Error('unhandled action type');
+  }
+}
 
 function Counter() {
-  const [count, setCount] = useState<number>(0);
+  const [count, dispatch] = useReducer(reducer, 0);
   // 제너릭 생략 가능 값(0)으로 유추
-  const onIncrease = () => setCount(count + 1);
-  const deIncrease = () => setCount(count - 1);
+  const onIncrease = () => dispatch({ type: 'INCREASE' });
+  const deIncrease = () => dispatch({ type: 'DECREASE' });
 
   return (
     <div>
