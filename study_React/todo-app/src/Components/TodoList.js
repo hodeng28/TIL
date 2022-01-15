de7a29f3365d675c3afc92a27855e0c8513641d3
hodeng28 <ghepd28@gmail.com>
2022-01-15 16:28:03 +0900

    feat: infinite scroll, react-virtualized

--- study_React/todo-app/src/Components/TodoList.js
+++ study_React/todo-app/src/Components/TodoList.js
@@ -1,22 +1,38 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { List } from 'react-virtualized';
 import './style/TodoList.scss';
 import TodoListItem from './TodoListItem';
 
 const TodoList = ({ todos, onRemove, onToggle }) => {
+  const rowRenderer = useCallback(
+    ({ index, key, style }) => {
+      const todo = todos[index];
+
+      return (
+        <TodoListItem
+          todo={todo}
+          key={key}
+          onRemove={onRemove}
+          onToggle={onToggle}
+          style={style}
+        />
+      );
+    },
+    [todos, onRemove, onToggle],
+  );
+
   return (
-    <>
-      <div className="todoList">
-        {todos.map((todo) => (
-          <TodoListItem
-            todo={todo}
-            key={todo.id}
-            onRemove={onRemove}
-            onToggle={onToggle}
-          />
-        ))}
-      </div>
-    </>
+    <List
+      className="todoList"
+      width={512}
+      height={513}
+      rowCount={todos.length}
+      rowHeight={57}
+      rowRenderer={rowRenderer}
+      list={todos}
+      style={{ outline: 'none' }}
+    />
   );
 };
 
-export default TodoList;
+export default React.memo(TodoList);
