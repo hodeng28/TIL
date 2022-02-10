b99b4ac52cf21b6fe49f961a036cd6654ca1ebe1
hodeng28 <ghepd28@gmail.com>
2022-02-11 00:13:46 +0900

    fix algorithm1

--- algorithm_study/programmers/Level2/algorithim1.js
+++ algorithm_study/programmers/Level2/algorithim1.js
@@ -3,17 +3,17 @@
 // 0 또는 양의 정수가 담긴 배열 numbers가 매개변수로 주어질 때, 순서를 재배치하여 만들 수 있는 가장 큰 수를 문자열로 바꾸어 return 하도록 solution 함수를 작성해주세요.
 
 function solution(numbers) {
-  const numbersToString = numbers.map((number) => String(number));
+  const numbersToString = numbers
+    .map((number) => number.toString())
+    .sort((a, b) => b + a - (a + b))
+    .join("");
   // console.log(numbersToString);
-  numbersToString.sort((a, b) => {
-    return b + a - (a + b);
-  });
 
-  const answer = numbersToString[0] === 0 ? "0" : numbersToString.join("");
+  const answer = numbersToString[0] === 0 ? "0" : numbersToString;
   return answer;
 }
 
 console.log(solution([6, 10, 2]));
 console.log(solution([3, 30, 34, 5, 9]));
 
-// 시간 초과로 개선 필요함
+// 시간 초과로.. 수정중..
