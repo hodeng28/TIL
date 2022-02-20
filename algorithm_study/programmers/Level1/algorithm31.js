a01912aa2e95fea5600d5b492328180655fa5501
hodeng28 <ghepd28@gmail.com>
2022-02-20 22:58:13 +0900

    algorithm31

---
+++ algorithm_study/programmers/Level1/algorithm31.js
@@ -0,0 +1,12 @@
+// 문자열로 구성된 리스트 strings와, 정수 n이 주어졌을 때, 각 문자열의 인덱스 n번째 글자를 기준으로 오름차순 정렬하려 합니다.
+// 예를 들어 strings가 ["sun", "bed", "car"]이고 n이 1이면 각 단어의 인덱스 1의 문자 "u", "e", "a"로 strings를 정렬합니다.
+
+function solution(strings, n) {
+  strings.sort((a, b) => {
+    return a[n] === b[n] ? (a > b) - (a < b) : (a[n] > b[n]) - (a[n] < b[n]);
+  });
+  return strings;
+}
+
+console.log(solution(["sun", "bed", "car"], 1));
+console.log(solution(["abce", "abcd", "cdx"], 2));
