7409221a8a222d82e06f10d8ff5b8107fd724d24
hodeng28 <ghepd28@gmail.com>
2022-03-08 00:39:25 +0900

    life cycle

--- study_React/lifecycle/src/App.js
+++ study_React/lifecycle/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import "./App.css";
+import LifeCycleSample from "./LifeCycleSample";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function getRandomColor() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
+
+class App extends Component {
+  state = {
+    color: "#000000",
+  };
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor(),
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <button onClick={this.handleClick}>random</button>
+        <LifeCycleSample color={this.state.color} />
+      </>
+    );
+  }
 }
 
 export default App;
--- study_React/lifecycle/src/LifeCycleSample.js
+++ study_React/lifecycle/src/LifeCycleSample.js
@@ -1,3 +1,5 @@
+// 라이프 사이클 메서드 이해하기(class형)
+
 import React, { Component } from "react";
 
 class LifeCycleSample extends Component {
@@ -12,4 +14,66 @@ class LifeCycleSample extends Component {
     super(props);
     console.log("constructor");
   }
+
+  static getDerivedStateFromProps(nextProps, prevState) {
+    console.log("getDerivedStateFromProps");
+    if (nextProps.color !== prevState.color) {
+      return { color: nextProps.color };
+    }
+    return null;
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount");
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("shouldComponentUpdate", nextProps, nextState);
+    return nextState.number % 10 !== 4;
+  }
+
+  componentWillUnmount() {
+    console.log("componentWillUnmount");
+  }
+
+  handleClick = () => {
+    this.setState({
+      number: this.state.number + 1,
+    });
+  };
+
+  getSnapshotBeforeUpdate(prevProps, nextState) {
+    console.log("getSnapshotBeforeUpdate");
+    if (prevProps.color !== this.props.color) {
+      return this.myRef.style.color;
+    }
+    return null;
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log("componentDidUpdate", prevProps, prevState);
+    if (snapshot) {
+      console.log("업데이트되기 전 색상", snapshot);
+    }
+  }
+
+  render() {
+    console.log("render");
+
+    const style = {
+      color: this.props.color,
+    };
+
+    return (
+      <>
+        <h1 style={style} ref={(ref) => (this.myRef = ref)}>
+          {this.state.number}
+        </h1>
+        <div>color: {this.state.color}</div>
+        <button onClick={this.handleClick}>+</button>
+      </>
+    );
+  }
 }
+
+export default LifeCycleSample;
