d5a4deeb7c209949a643c7f823c0f7c3b36f88fd
hodeng28 <ghepd28@gmail.com>
2022-03-10 23:34:17 +0900

    useContext

--- study_React/context-api/src/Compontents/ColorBox.js
+++ study_React/context-api/src/Compontents/ColorBox.js
@@ -1,29 +1,59 @@
-import { ColorConsumer } from "../Contexts/color";
+import { useContext } from "react";
+import ColorContext from "../Contexts/color";
 
 const ColorBox = () => {
+  // Consumer 대신 useContext Hooks 사용
+  const { state } = useContext(ColorContext);
   return (
-    // ColorContext 안에 들어있는 Consumer라는 컴포넌트롤 통해 색상을 조회
-    <ColorConsumer>
-      {({ state }) => (
-        <>
-          <div
-            style={{
-              width: "64px",
-              height: "64px",
-              background: state.color,
-            }}
-          ></div>
-          <div
-            style={{
-              width: "32px",
-              height: "32px",
-              background: state.subcolor,
-            }}
-          ></div>
-        </>
-      )}
-    </ColorConsumer>
+    <>
+      <div
+        style={{
+          width: "64px",
+          height: "64px",
+          background: state.color,
+        }}
+      ></div>
+      <div
+        style={{
+          width: "32px",
+          height: "32px",
+          background: state.subcolor,
+        }}
+      ></div>
+    </>
   );
 };
 
 export default ColorBox;
+
+// import { ColorConsumer } from "../Contexts/color";
+
+// const ColorBox = () => {
+
+// children에 함수를 전달하는 Render Props 패턴
+//   return (
+//     // ColorContext 안에 들어있는 Consumer라는 컴포넌트롤 통해 색상을 조회
+//     <ColorConsumer>
+//       {({ state }) => (
+//         <>
+//           <div
+//             style={{
+//               width: "64px",
+//               height: "64px",
+//               background: state.color,
+//             }}
+//           ></div>
+//           <div
+//             style={{
+//               width: "32px",
+//               height: "32px",
+//               background: state.subcolor,
+//             }}
+//           ></div>
+//         </>
+//       )}
+//     </ColorConsumer>
+//   );
+// };
+
+// export default ColorBox;
