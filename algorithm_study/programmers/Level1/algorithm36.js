547e097ab258a54b58a469724318473022be1a53
hodeng28 <ghepd28@gmail.com>
2022-04-01 22:26:12 +0900

    algorithm36

---
+++ algorithm_study/programmers/Level1/algorithm36.js
@@ -0,0 +1,27 @@
+// 중복되는 수 제거
+//
+
+function solution(nums) {
+  let answer = [];
+  const maxChoice = nums.length;
+
+  for (let i = 0; i < maxChoice; i++) {
+    if (answer.indexOf(nums[i]) === -1) {
+      answer.push(nums[i]);
+    }
+    if (answer.length >= maxChoice / 2) {
+      return answer.length;
+    }
+  }
+  return answer.length;
+}
+
+console.log(solution([3, 1, 2, 3]));
+console.log(solution([3, 3, 3, 2, 2, 4]));
+console.log(solution([3, 3, 3, 2, 2, 2]));
+
+// 다른사람들의 풀이.
+// const max = nums.length / 2;
+// const arr = [...new Set(nums)];    new Set 활용..
+
+// return arr.length > max ? max : arr.length
