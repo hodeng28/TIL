72077d9b631014ddcf397ee3fef81a9cc032a0be
hodeng28 <ghepd28@gmail.com>
2022-04-09 01:06:07 +0900

    feat: login page

--- study_React/todo-zustand/src/Components/Login.jsx
+++ study_React/todo-zustand/src/Components/Login.jsx
@@ -8,12 +8,15 @@ const Login = () => {
         <div className="login-input-wrap">
           <input 
             type="text" 
-            placeholder="아이디를 입력해주세요"
+            placeholder="아이디를 입력해주세요."
           />
           <input
              type="text"
-             placeholder="비밀번호를 입력해주세요"
+             placeholder="비밀번호를 입력해주세요."
           />
+          <button>
+            로그인
+          </button>
         </div>
       </div>
     </>
