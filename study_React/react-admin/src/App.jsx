219a0474e82ac12a5c61e7e40ac697e4eed0c088
hodeng28 <ghepd28@gmail.com>
2022-05-12 23:51:23 +0900

    init react-admin

--- study_React/react-admin/src/App.jsx
+++ study_React/react-admin/src/App.jsx
@@ -1,45 +1,13 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import * as React from "react";
+import { Admin, Resource, ListGuesser } from 'react-admin';
+import jsonServerProvider from 'ra-data-json-server';
 
-function App() {
-  const [count, setCount] = useState(0)
+const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
-}
+const App = () => (
+    <Admin dataProvider={dataProvider}>
+        <Resource name="users" list={ListGuesser} />
+    </Admin>
+  );
 
-export default App
+export default App;
