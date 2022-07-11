2fdaeb43f6faa7210c299f926c5b71e0d8822bcb
hodeng28 <ghepd28@gmail.com>
2022-07-12 00:27:38 +0900

    change folder name

---
+++ study_Next.js/pages/about.tsx
@@ -0,0 +1,14 @@
+import styles from "../styles/Home.module.css";
+// import { Stack, Button } from "@mui/material";
+import Seo from "./Components/Seo";
+
+const About = () => {
+  return (
+    <div className={styles.container}>
+      <Seo title="about" />
+      <h1>about</h1>
+    </div>
+  );
+};
+
+export default About;
