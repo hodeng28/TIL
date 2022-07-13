90cbbdc43d31d42e8977bfd88c225f70d28d48b9
hodeng28 <ghepd28@gmail.com>
2022-07-13 23:54:41 +0900

    feat: next.js redirect

--- study_Next.js/pages/index.tsx
+++ study_Next.js/pages/index.tsx
@@ -1,12 +1,67 @@
 import type { NextPage } from "next";
 import Seo from "./Components/Seo";
-import styles from "../styles/Home.module.css";
+import { useEffect, useState } from "react";
+
+interface IMovieProps {
+  id: number;
+  backdrop_path: string;
+  original_title: string;
+  overview: string;
+  poster_path: string;
+  title: string;
+  vote_average: number;
+  genre_ids: [number];
+}
 
 const Home: NextPage = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const { results } = await (
+        await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+        )
+      ).json();
+      setMovies(results);
+      console.log(results);
+    })();
+  }, []);
+
   return (
-    <div className={styles.container}>
+    <div className="container">
       <Seo title="Home" />
-      <h1>Home</h1>
+      {!movies && <h4>Loading</h4>}
+      {movies?.map((movie: IMovieProps) => (
+        <div key={movie.id} className="movie">
+          <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
+      <style jsx>{`
+        .container {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          padding: 20px;
+          gap: 20px;
+        }
+        .movie {
+          cursor: pointer;
+        }
+        .movie img {
+          max-width: 100%;
+          border-radius: 12px;
+          transition: transform 0.2s ease-in-out;
+          box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+        }
+        .movie:hover img {
+          transform: scale(1.05) translateY(-10px);
+        }
+        .movie h4 {
+          font-size: 18px;
+          text-align: center;
+        }
+      `}</style>
     </div>
   );
 };
