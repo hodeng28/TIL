9884804f0ad1fe433c7255c1669385b95e80dc5d
hodeng28 <ghepd28@gmail.com>
2022-08-10 01:20:59 +0900

    web3 init

--- web3-react/src/main.tsx
+++ web3-react/src/main.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
+import { Web3ReactProvider } from "@web3-react/core";
+import { Web3Provider } from "@ethersproject/providers";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+const getLibrary = (provider: any) => {
+  const library = new Web3Provider(provider, "any");
+  return library;
+};
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <StrictMode>
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <App />
+    </Web3ReactProvider>
+  </StrictMode>,
+  rootElement
+);
