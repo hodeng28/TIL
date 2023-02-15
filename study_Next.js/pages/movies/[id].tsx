813ae1d462bd4777ede6f0f12b390e134d85408b
hodeng28 <ghepd28@gmail.com>
2023-02-15 19:45:58 +0900

    feat: pass data using router query

--- study_Next.js/pages/movies/[id].tsx
+++ study_Next.js/pages/movies/[id].tsx
@@ -1,7 +1,21 @@
+import Image from "next/image";
 import { useRouter } from "next/router";
 
 const Details = () => {
   const router = useRouter();
-  return <div>{router.query.title || <h4>loading</h4>}</div>;
+  console.log(router);
+  const { title, poster_path, overview } = router.query;
+  return (
+    <>
+      <div>{title || <h4>loading</h4>}</div>
+      <Image
+        src={`https://image.tmdb.org/t/p/w500${poster_path}`}
+        width={500}
+        height={400}
+        alt={`${title} poster`}
+      />
+      <div>{overview}</div>
+    </>
+  );
 };
 export default Details;
