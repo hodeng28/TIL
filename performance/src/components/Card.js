3628db6c90571b6b25fae268d9a779bcb60da253
hodeng28 <ghepd28@gmail.com>
2023-03-05 21:28:19 +0900

    init

---
+++ performance/src/components/Card.js
@@ -0,0 +1,32 @@
+import { useEffect, useRef } from "react";
+
+const Card = (props) => {
+  const imgRef = useRef(null);
+
+  useEffect(() => {
+    const options = {};
+    const callback = (entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.src = entry.target.dataset.src;
+          observer.unobserve(entry.target);
+        }
+      });
+    };
+
+    const observer = new IntersectionObserver(callback, options);
+
+    observer.observe(imgRef.current);
+  });
+
+  return (
+    <div className="Card text-center">
+      <img data-src={props.image} ref={imgRef} />
+      <div className="p-5 font-semibold text-gray-700 text-xl md:text-lg lg:text-xl keep-all">
+        {props.children}
+      </div>
+    </div>
+  );
+};
+
+export default Card;
