aa71128ad9dcd466a39881f771693e57c8410b68
hodeng28 <ghepd28@gmail.com>
2023-03-11 23:22:52 +0900

    use iamge webp

--- performance/src/components/Card.js
+++ performance/src/components/Card.js
@@ -8,7 +8,12 @@ const Card = (props) => {
     const callback = (entries, observer) => {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
-          entry.target.src = entry.target.dataset.src;
+          const target = entry.target;
+          const previousSibling = target.previousSibling;
+
+          target.src = target.dataset.src;
+          previousSibling.srcset = previousSibling.dataset.srcset;
+
           observer.unobserve(entry.target);
         }
       });
@@ -21,7 +26,10 @@ const Card = (props) => {
 
   return (
     <div className="Card text-center">
-      <img data-src={props.image} ref={imgRef} />
+      <picture>
+        <source data-srcset={props.webp} type="image/webp"></source>
+        <img data-src={props.image} ref={imgRef} />
+      </picture>
       <div className="p-5 font-semibold text-gray-700 text-xl md:text-lg lg:text-xl keep-all">
         {props.children}
       </div>
