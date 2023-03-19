5f0015f95d6f0d4f4468f335fdb6ff7ed3fda5a9
hodeng28 <ghepd28@gmail.com>
2023-03-19 21:17:54 +0900

    remove code

--- study_React/shopping-mall/src/pages/cart/index.tsx
+++ study_React/shopping-mall/src/pages/cart/index.tsx
@@ -2,13 +2,6 @@ import { useQuery } from "react-query";
 import { GET_CART } from "../../graphql/cart";
 import { cartAddFetcher, QueryKeys } from "../../queryClient";
 
-amount: 3;
-createdAt: "Tue Mar 07 2023 23:09:09 GMT+0900 (한국 표준시)";
-description: "상세내용1";
-id: "1";
-imageUrl: "https://placeimg.com/640/480/1";
-price: 50000;
-title: "상품1";
 const Cart = () => {
   const { data } = useQuery(QueryKeys.CART, () => cartAddFetcher(GET_CART));
   if (!data) {
